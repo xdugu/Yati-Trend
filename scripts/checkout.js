f65885aff2140eba0917b207e4330730c3652494
xdugu <xdugu2007@gmail.com>
2019-05-18 21:24:58 +0200

    Added foxpost integration in checkout
    Need to tidy up

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,18 +2,31 @@
 var scrollingElement = (document.scrollingElement || document.body);
 
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngAnimate']);
 app.run(function() {
 // Trigger input event on change to fix auto-complete
 $('input, select').on('change',function() { $(this).trigger('input'); });
 });
-app.controller('Checkout', function($scope, $http, $timeout) {
+app.controller('Checkout',  function($scope, $http, $timeout) {
 	order = localStorage.getObj("order");
-	$scope.user=order.contact;
+	$scope.user=[];
+	$scope.places =[];
+	$http.get("/convertcsv.json").then(function(resp){
+		$scope.places = resp.data;
+	});
 	
 	$scope.proceedToPayment= function (){
 		Shop_updateContact($scope.user);
 		window.location.href = 'review.html';
+	}
+	
+	$scope.foxpostChosenLoc = function(place){
+		$scope.user.address1 = "Foxpost";
+		$scope.user.address2 = place.Address;
+		$scope.user.city = place.City;
+		$scope.user.postCode = place.Address.substring(0,4);
+		$scope.showAddr =false;
+		
 	}
 
 	$scope.$watch('agreed',function(newValue, oldValue) {
--- scripts/directives/directives.js
+++ scripts/directives/directives.js
@@ -1,37 +1,5 @@
+﻿
 
-/*angular.module('myApp').directive('myCostStr', function() {
-  return {
-  scope: {
-      cost: '=cost'
-    },
-	template:'{{convert(cost)}}',
-    link: function($scope,elem, attr) {
-	$scope.convert=function (myCost){
-		if(myCost==null)
-		{
-			return 'I need a "cost" attribute to work';
-		}
-		if(myCost<1000)
-		{
-			myCost=myCost.toString();
-		}
-		else
-		{
-			let partBeforePoint = Math.trunc(myCost/1000);
-			let partAfterPoint = myCost - partBeforePoint * 1000;
-			if(partAfterPoint<10)
-			myCost= partBeforePoint.toString() +'.00'+ partAfterPoint.toString();
-			else if(partAfterPoint<100)
-				myCost= partBeforePoint.toString() +'.0'+ partAfterPoint.toString();
-			else
-				myCost= partBeforePoint.toString() +'.'+ partAfterPoint.toString();
-		}
-      return myCost+" Ft";
-	 
-	  }
-    }
-  };
-});*/
 angular.module('myApp').directive('myCostStr', function() {
   return {
 	restrict: 'A',
@@ -41,28 +9,72 @@ angular.module('myApp').directive('myCostStr', function() {
 			//let myCost=parseInt(attr.myCostStr);
 			if(myCost==null)
 			{
-				return 'I need a "cost" attribute to work';
+				return '';
 			}
-			if(myCost<1000)
-			{
-				myCost=myCost.toString();
+			
+			if($scope.currency=="EUR"){
+				myCost = "€" + Number.parseFloat(myCost).toFixed(2);
+				myCost+= " EUR";
+				
 			}
-			else
-			{
-				let partBeforePoint = Math.trunc(myCost/1000);
-				let partAfterPoint = myCost - partBeforePoint * 1000;
-				if(partAfterPoint<10)
-				myCost= partBeforePoint.toString() +'.00'+ partAfterPoint.toString();
-				else if(partAfterPoint<100)
-					myCost= partBeforePoint.toString() +'.0'+ partAfterPoint.toString();
+			else {
+				if(myCost<1000)
+				{
+					myCost=myCost.toString();
+				}
 				else
-					myCost= partBeforePoint.toString() +'.'+ partAfterPoint.toString();
+				{
+					let partBeforePoint = Math.trunc(myCost/1000);
+					let partAfterPoint = myCost - partBeforePoint * 1000;
+					if(partAfterPoint<10)
+					myCost= partBeforePoint.toString() +'.00'+ partAfterPoint.toString();
+					else if(partAfterPoint<100)
+						myCost= partBeforePoint.toString() +'.0'+ partAfterPoint.toString();
+					else
+						myCost= partBeforePoint.toString() +'.'+ partAfterPoint.toString();
+				}
+				myCost+=" Ft";
 			}
-		  return myCost+" Ft";	
+		  return myCost;	
 		}
 		$scope.$watch(attr.myCostStr, function(value) {
-			elem.text(calc(value));
+				elem.text(calc(value));
 		});
     }
   };
+});
+
+angular.module('myApp').filter('accentsfilter',function(){
+	function removeAccents(value) {
+		return value
+            .replace(/á/g, 'a')            
+            .replace(/é/g, 'e')
+            .replace(/í/g, 'i')
+            .replace(/ó/g, 'o')
+			.replace(/ö/g, 'o')
+			.replace(/ő/g, 'o')
+			.replace(/ű/g, 'u')
+            .replace(/ú/g, 'u')
+			.replace(/ü/g, 'u')
+    }
+
+    return function(items, filter) {               
+        if (!filter) return items;       
+		var finalArr=[];
+		
+		items.forEach(function(item){			
+			keys = Object.values(item);//expecting an object so will loop through the properties
+			for (const key of keys) {
+				var text = removeAccents(key.toLowerCase())
+				filter = removeAccents(filter.toLowerCase());
+				if(text.indexOf(filter) > -1){
+					finalArr.push(item);
+					break;
+				}
+			}
+		});
+		
+		return finalArr;
+    };
+	
 });
