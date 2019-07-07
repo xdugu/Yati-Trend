756b442f1821772ef5899eec635b8dafd6eb156d
xdugu <xdugu2007@gmail.com>
2019-07-07 08:37:14 +0200

    Started Coming soon page

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,8 @@
   
-var app = angular.module('myApp', ['slickCarousel']);
+var app = angular.module('myApp', []);
 app.controller('HomePage', function($http,$scope) {
-
-$scope.slickConfig={
-				dots: false,
-				arrows:false,
-				infinite: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				autoplaySpeed: 3500,
-				pauseOnHover: false,
-				pauseOnFocus:false,
-				speed:2000,
-				fade:true,
-				swipe : false,
-				lazyLoad: 'ondemand',
-				responsive: [
-				{
-					breakpoint: 480, // mobile breakpoint
-					settings: {
-						slidesToShow: 1,
-					}
-				}
-				]		
-			  };
+	document.getElementById("background_img").style.opacity="0.9";
+	$("#text").css({"max-height": "1000px"});
 
 });
 
