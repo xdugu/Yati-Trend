e3ac62bc89b9c011c5ac514d246ecd82144606a7
xdugu <xdugu2007@gmail.com>
2019-07-07 18:38:42 +0200

    Stable version with video for different screens

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,31 @@
   
 var app = angular.module('myApp', []);
-app.controller('HomePage', function($http,$scope) {
-	document.getElementById("background_img").style.opacity="0.9";
+app.controller('HomePage', function($http,$scope,$timeout) {
+	$scope.contentSeen = true;
+	
+	$scope.rowUp = function(){
+		
+		if($scope.contentSeen){
+			$("#text").css({"max-height": "0px"});
+			$("#actual_arrow").text("expand_more");
+			$scope.contentSeen = false;
+			
+			
+		}else{
+			$("#text").css({"max-height": "1000px"});
+			$("#actual_arrow").text("expand_less");
+			
+			$scope.contentSeen = true;
+		}
+		
+	}
+	
+	$timeout(function(){
+	
 	$("#text").css({"max-height": "1000px"});
+	$("#arrow").css({"visibility": "visible"});
+		
+	},5000);
 
 });
 
