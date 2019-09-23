9a016e31ca55c5591dc2ac3b17b8d255a880a3ab
xdugu <xdugu2007@gmail.com>
2019-09-23 22:08:02 +0200

    blog stuff

---
+++ scripts/v1/blog.js
@@ -0,0 +1,37 @@
+//The functions below allow us to store arrays in local storage to be used later
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+var scope;
+
+var app = angular.module('myApp', ['ngSanitize']);
+
+
+
+app.controller('BlogList', function($scope, $http, $timeout, $location, $window) {
+	$scope.products=[];
+	$scope.urlParams = Common_parseUrlParam();
+	//Language stuff
+	$scope.backbone = {lang:null,loading:true};
+	$scope.backbone.lang= localStorage.getObj("shopping").contact.lang;//for choosing of language	
+	$scope.blogList = [];
+
+	
+
+	
+	/////////////////////////////////
+	$http.get('https://api.yati-trend.com/v1/Request/Blog/GetBlogList').then(function(res){
+			
+	    $scope.blogList = res.data.data;
+		
+	});
+	
+
+});
+
+
+
