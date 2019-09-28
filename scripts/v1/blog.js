db09855c5fa35554f1f7b9991f47ba4b8008588b
xdugu <xdugu2007@gmail.com>
2019-09-28 12:23:04 +0200

    Uploaded blog

--- scripts/v1/blog.js
+++ scripts/v1/blog.js
@@ -36,10 +36,25 @@ app.controller('BlogList', function($scope, $http, $timeout, $location, $window)
 app.controller('BlogView', function($scope, $http, $timeout, $location, $window) {
 	$scope.products=[];
 	$scope.urlParams = Common_parseUrlParam();
+	$scope.title = '';
 	//Language stuff
 	$scope.backbone = {lang:null,loading:true};
 	$scope.backbone.lang= localStorage.getObj("shopping").contact.lang;//for choosing of language	
 	$scope.blogList = [];
+	
+	if($scope.urlParams.id !=null){
+		$http.get('https://api.yati-trend.com/v1/Request/Blog/GetBlogList').then(function(res){
+				    $scope.blogList = res.data.data;
+		for(let i=0; i< $scope.blogList.length; i++){
+			if($scope.blogList[i].id == parseInt($scope.urlParams.id)){
+				$scope.title = $scope.blogList[i][$scope.backbone.lang]['title'];
+				break;
+			}
+		}
+		
+	});
+		
+	}
 
 	
 
